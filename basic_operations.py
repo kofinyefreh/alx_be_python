c137f83f56a03592e9b8a97da3f0d8a6cb4952cc
Kofi Nyefreh <nyefrehkofi@gmail.com>
2025-07-12 11:29:43 -0700

    Add basic arithemetic operations logic

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+# Define variables
+number1 = 10
+number2 = 5
+
+# sum
+sumNo = number1 + number2
+print (f'Addition of {number1} and {number2} is {sumNo}')
+
+# difference
+difference = number2 - number1
+print (f'Subtraction of {number2} and {number1} is {difference}')
+
+# multiplication
+product = number1 * number2
+print (f'Multiplication of {number1} and {number2} is {product}')
+
