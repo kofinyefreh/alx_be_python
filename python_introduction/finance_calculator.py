bc5140938ab234de14c61bcea012f2785d3fc740
Kofi Nyefreh <nyefrehkofi@gmail.com>
2025-07-12 12:43:57 -0700

    final bug fix and cleanup

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
-monthly_income = float(input('Enter monthly income: '))
-monthly_expenses = float(input('Enter total expenses: '))
+monthly_income = float(input('Enter your monthly income: '))
+monthly_expenses = float(input('Enter your total monthly expenses: '))
 
 monthly_savings = monthly_income - monthly_expenses
 
@@ -7,4 +7,5 @@ projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 print (f'Your monthly savings are ${monthly_savings:.0f}')
 print (f'Projected saving after one year, with interest, is: ${projected_savings:.0f}')
+
 
