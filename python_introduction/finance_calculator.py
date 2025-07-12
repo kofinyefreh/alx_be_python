8242bec85512748487a65b10423ca6c060cfe19f
Kofi Nyefreh <nyefrehkofi@gmail.com>
2025-07-12 12:38:16 -0700

    fix bug test two

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,20 @@
-monthlyIncome = input('Enter monthly income: ')
-monthlyExpenses = float(input('Enter total expenses: '))
+# monthly_income = float(niput('Enter monthly income: '))
+# monthly_expenses = float(input('Enter total expenses: '))
 
-monthlySavings = monthlyIncome - monthlyExpenses
+# monthly_savings = monthly_income - monthly_expenses
 
-projectedSavings = (monthlySavings * 12 + (monthlySavings * 12 * 0.05))
+# projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
-print (f'Your monthly savings are ${monthlySavings}')
-print (f'Projected saving after one year, with interest, is: ${projectedSavings}')
+# print (f'Your monthly savings are ${monthlySavings}')
+# print (f'Projected saving after one year, with interest, is: ${projectedSavings}')
+
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+annual_savings = monthly_savings * 12
+interest = annual_savings * 0.05
+projected_savings = annual_savings + interest
+
+print(f"Your monthly savings are ${monthly_savings:.0f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.0f}.")
