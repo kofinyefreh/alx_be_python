a9879ef3b40dd12b611726583411881ea0b6db67
Kofi Nyefreh <nyefrehkofi@gmail.com>
2025-07-12 12:09:33 -0700

    Add and test future age calcuation file

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+age = input('How old ar you?')
+currentYear = 2023
+futureYear = 2050
+
+additionalYears = futureYear - currentYear
+totalYears = additionalYears + age
+
+print (f'In {futureYear}, you will be {age} years old')
